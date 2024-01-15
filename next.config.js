3490181dd66f4a86d4bea29eeb058b89bd4c37f5
rijalghodi <rijalghodi10@gmail.com>
2024-01-15 17:57:29 +0700

    add module federation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,40 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "main",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {},
+        remotes: {
+          dashboard: `dashboard@http://localhost:3001/_next/static/${
+            isServer ? "ssr" : "chunks"
+          }/remoteEntry.js`,
+        },
+        shared: {
+          "next/router": {
+            singleton: true,
+            requiredVersion: false,
+          },
+        },
+      })
+    );
+
+    return config;
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
